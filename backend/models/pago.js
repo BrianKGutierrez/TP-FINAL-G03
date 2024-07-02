c9a2cb8c30b40fb3276c60f45345c8df5160fe28
NatanaelDominguez <jorgedominguez349@gmail.com>
2024-07-02 19:28:41 -0300

    actualización en el modelo de pago

--- backend/models/pago.js
+++ backend/models/pago.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-//const Alquiler = require('./alquiler');
+const Alquiler = require('./alquiler');
+const Adelanto = require('./adelanto');
 
 const PagoSchema = new Schema({
-    medioPago: {type: String, required: true},
-    fechaPago: {type: Date, required: true},
-    monto: {type: Number, required: true}
+    alquiler: {type: Schema.Types.ObjectId, ref:Alquiler, required: true},
+    nroCuota: {type: Number, required: true},
+    monto: {type: Number, required: true},
+    tipoDePago: {type: String, required: true},
+    fechaDePago: {type: Date, required: true},
+    estado: {type: Boolean, required: true},
+    adelantos: [{type: Adelanto.schema, required: true}]
 });
 
 module.exports = mongoose.models.Pago || mongoose.model('Pago', PagoSchema);
