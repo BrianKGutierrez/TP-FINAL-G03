ecab8ae6397553d316b7d25ae302d7259667ab2d
Nadia Burgos <nadiaburgos.7@gmail.com>
2024-07-05 14:06:24 -0300

    Lista y Formulario para Propietario con servicio y clase (FUNCIONAL)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { PropietarioComponent } from './components/propietario/propietario.component';
+import { FormPropietarioComponent } from './components/form-propietario/form-propietario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'propietario', component : PropietarioComponent
+        
+    },
+    {
+        path: 'formpropietario/:id', component : FormPropietarioComponent
+        
+    }
+
+];
