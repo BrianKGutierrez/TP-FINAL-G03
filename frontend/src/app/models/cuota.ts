c4d54efbdbee7852cd4baef98e89bcf2e9b3dedb
NatanaelDominguez <jorgedominguez349@gmail.com>
2024-07-08 14:27:05 -0300

    modelo de cuota para frontend

--- frontend/src/app/models/cuota.ts
+++ frontend/src/app/models/cuota.ts
@@ -1,2 +1,12 @@
+import { Alquiler } from "./alquiler";
+import { Pago } from "./pago";
+
 export class Cuota {
+    _id!: string;
+    alquiler!:Alquiler;
+    mes!:number;
+    monto!:number;
+    nroCuota!:number;
+    adelantos!:Array<Pago>;
+    estado!:boolean;
 }
