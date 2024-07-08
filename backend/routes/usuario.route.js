d3a8f1c4f38ac04ab205b7ddfb622f43f7c5ef5d
BrianKGutierrez <brian21klg@gmail.com>
2024-07-08 02:24:15 -0300

    registrar usuario-falta corregir agregar y eliminar me salata error menos update eso funciona

--- backend/routes/usuario.route.js
+++ backend/routes/usuario.route.js
@@ -4,12 +4,12 @@ const usuarioCtrl = require('../controllers/usuario.controller');
 const autCtrl = require('./../controllers/auth.controller');//necesario para el auth
 
 // Rutas para CRUD de usuarios
-router.post('/', autCtrl.verifyToken,usuarioCtrl.createUsuario); // Crear usuario
+router.post('/',usuarioCtrl.createUsuario); // Crear usuario
 router.post('/login', usuarioCtrl.loginUsuario); // Login de usuario
 
-router.get('/', autCtrl.verifyToken,usuarioCtrl.getUsuarios); // Obtener todos los usuarios
-router.get('/:id',autCtrl.verifyToken, usuarioCtrl.getUsuario); // Obtener usuario por ID
-router.put('/:id',autCtrl.verifyToken, usuarioCtrl.editUsuario); // Actualizar usuario por ID
-router.delete('/:id',autCtrl.verifyToken, usuarioCtrl.deleteUsuario); // Eliminar usuario por ID
+router.get('/',usuarioCtrl.getUsuarios); // Obtener todos los usuarios
+router.get('/:id', usuarioCtrl.getUsuario); // Obtener usuario por ID
+router.put('/:id', usuarioCtrl.editUsuario); // Actualizar usuario por ID
+router.delete('/:id', usuarioCtrl.deleteUsuario); // Eliminar usuario por ID
 
 module.exports = router;
