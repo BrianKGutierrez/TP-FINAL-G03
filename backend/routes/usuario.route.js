cdd3454b193f05bbed9cbc0d026f2fda22239062
BrianKGutierrez <brian21klg@gmail.com>
2024-07-07 23:22:08 -0300

    modificaciones en route user

--- backend/routes/usuario.route.js
+++ backend/routes/usuario.route.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const usuarioCtrl = require('../controllers/usuario.controller');
-const authCtrl = require('../controllers/auth.controller');
+const autCtrl = require('./../controllers/auth.controller');//necesario para el auth
 
 // Rutas para CRUD de usuarios
-router.post('/', authCtrl.verifyToken, usuarioCtrl.createUsuario); // Crear usuario
+router.post('/', autCtrl.verifyToken,usuarioCtrl.createUsuario); // Crear usuario
 router.post('/login', usuarioCtrl.loginUsuario); // Login de usuario
 
-router.get('/', usuarioCtrl.getUsuarios); // Obtener todos los usuarios
-router.get('/:id', authCtrl.verifyToken, usuarioCtrl.getUsuario); // Obtener usuario por ID
-router.put('/:id', authCtrl.verifyToken, usuarioCtrl.editUsuario); // Actualizar usuario por ID
-router.delete('/:id', authCtrl.verifyToken, usuarioCtrl.deleteUsuario); // Eliminar usuario por ID
+router.get('/', autCtrl.verifyToken,usuarioCtrl.getUsuarios); // Obtener todos los usuarios
+router.get('/:id',autCtrl.verifyToken, usuarioCtrl.getUsuario); // Obtener usuario por ID
+router.put('/:id',autCtrl.verifyToken, usuarioCtrl.editUsuario); // Actualizar usuario por ID
+router.delete('/:id',autCtrl.verifyToken, usuarioCtrl.deleteUsuario); // Eliminar usuario por ID
 
 module.exports = router;
