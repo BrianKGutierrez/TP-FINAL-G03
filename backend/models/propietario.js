dbb53b55ffa488e9401dfbee03fc5d05cbd23a4f
Nadia Burgos <nadiaburgos.7@gmail.com>
2024-07-11 15:41:50 -0300

    Se modifica relacion entre Usuario y Propietario

--- backend/models/propietario.js
+++ backend/models/propietario.js
@@ -6,6 +6,7 @@ const PropietarioSchema = new Schema({
     dni: { type: String, required: true },
     email: { type: String, required: true },
     telefono: { type: Number, required: true },
+    idUsuario: { type: Schema.Types.ObjectId, ref: 'Usuario', required: true }
 })
 module.exports = mongoose.models.Propietario || mongoose.model('Propietario', PropietarioSchema);
 
